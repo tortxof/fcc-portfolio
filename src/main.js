069f08a8d4ce7b744eafacab69149e32d9149a54
Daniel Jones <tortxof@gmail.com>
2016-07-06 00:02:19 -0400

    Fix nav target positions.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ function resetHeights() {
   var window_height = $(window).height();
   var nav_height = $('nav').height();
   $('.section-home').css('margin-top', nav_height);
-  $('.nav-target').css('top', -nav_height);
+  $('.nav-target').css('transform', 'translateY(-' + nav_height + 'px)');
   $('section').height(window_height - nav_height);
 }
 
