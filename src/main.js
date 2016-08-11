f26ee92d6d395e32843f77d6821b6930c1902627
Daniel Jones <tortxof@gmail.com>
2016-08-11 02:16:25 -0400

    Submit contact form with ajax.

--- src/main.js
+++ src/main.js
@@ -9,3 +9,33 @@ function resetHeights() {
 resetHeights();
 
 $(window).resize(resetHeights);
+
+$('#submit').click(function(event) {
+  event.preventDefault();
+
+  var xhr = new XMLHttpRequest();
+  var formData = new FormData($('#contact-form')[0]);
+
+  xhr.addEventListener('load', function() {
+    console.log('form posted successfully');
+    showContactModal();
+    setTimeout(hideContactModal, 2000);
+  });
+
+  xhr.addEventListener('error', function() {
+    console.log('error posting form');
+    showContactModal();
+    setTimeout(hideContactModal, 2000);
+  });
+
+  xhr.open('POST', $('#contact-form')[0].action);
+  xhr.send(formData);
+});
+
+function showContactModal() {
+  $('#contact-success-modal').addClass('show');
+}
+
+function hideContactModal() {
+  $('#contact-success-modal').removeClass('show');
+}
